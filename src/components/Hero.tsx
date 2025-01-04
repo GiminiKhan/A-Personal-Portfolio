50ff47a3d35e3a3ad0b0398fcb6736bee78a71d7
GiminiKhan <160707616+GiminiKhan@users.noreply.github.com>
2025-01-04 22:23:54 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,12 +4,12 @@ import Navbar from './Navbar'
 const Hero = () => {
 
   return (
-    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-left lg:bg-[12%] bg-cover'
+    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-left lg:bg-[15%] bg-cover'
       style={{backgroundSize: "20%"}}
       >
       <Navbar   />
 
-      <div className='container lg:grid-cols-2 h-[calc(100vh-60px)]'> 
+      <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'> 
         <div className='hidden lg:block'></div>
         <div className='text-[60px] sm:text[70px] font-bold leading-tight flex justify-center items-center '>
           <div>
@@ -25,3 +25,4 @@ const Hero = () => {
 }
 
 export default Hero
+
